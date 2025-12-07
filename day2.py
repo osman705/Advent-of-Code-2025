3a6d7bc6b632f1116c0ca63a7d44a78fa162e2d6
osman705 <ohassan705@gmail.com>
2025-12-07 11:38:56 +0000

    Completed day 2 part 1.
    Refactor ID splitting and validation logic; add repeated digit check.

--- day2.py
+++ day2.py
@@ -1,11 +1,13 @@
-"""
+import doctest
+
+
 with open ("day2-datafile.txt") as file:
     data = file.read()
 
 
 #print(data)
 
-"""
+
 
 
 test_data = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862"
@@ -15,22 +17,18 @@ test_data = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1
 # Format is 11-22 is the first id next id is followed by a comma
 
 #The job is to find all of the invalid IDs that appear in the given ranges.
-# Valid id == anything leading with 0 e.g 0101 or anything with a repeated digit within the range i.e from 11-22 we have 11 , 22 as invalid digits. 
+# You can find the invalid IDs by looking for any ID which is made only of some sequence of digits repeated twice. 
+# So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs. 
 
-
-
-
-"""
-Steps i'm thinking currently:
-
-1) split data by commas to get seperate IDs - Done
-2) find a way to read the individual lines up until the - seperatator to retrieve my first value, everything after the comma is my second value - Done
-3) Check if the first or second value starts with 0 if not then:
-3) loop through all values in the first value & second value for each ID 
-"""
 
 def split_id(data):
-    """ Split data into seperate IDs"""
+    """ Split data into seperate IDs
+    :input param: (data): Data is a long list of strings in the format "11-22,95-115,998-1012,1188511880-1188511890"
+    :returns: returns a list of 3-tuples (list_of_id_components) in the format [('11','-','22'), ('95','-','115'), ('998','-','1012')]
+
+    >>> split_id(test_data)
+    [('11', '-', '22'), ('95', '-', '115'), ('998', '-', '1012'), ('1188511880', '-', '1188511890'), ('222220', '-', '222224'), ('1698522', '-', '1698528'), ('446443', '-', '446449'), ('38593856', '-', '38593862')]"""
+    
     id_list = test_data.split(",")
     list_of_id_components = []
 
@@ -42,5 +40,66 @@ def split_id(data):
     return list_of_id_components
 
 
+def better_split_id(data):
+    """
+    Same functionality as split_id - more pythonic a as a list comprehension. 
+    
+    :input param: (data): Data is a long list of strings in the format "11-22,95-115,998-1012,1188511880-1188511890"
+    :returns: returns a list of 3-tuples in the format [('11','-','22'), ('95','-','115'), ('998','-','1012')]
+
+    >>> better_split_id(test_data)
+    [('11', '-', '22'), ('95', '-', '115'), ('998', '-', '1012'), ('1188511880', '-', '1188511890'), ('222220', '-', '222224'), ('1698522', '-', '1698528'), ('446443', '-', '446449'), ('38593856', '-', '38593862')]
+    """
+
+    return  [id.partition("-") for id in data.split(",")]
 
 
+"""
+Steps i'm thinking currently:
+
+Use pytest for this function with asserts for practise.
+1) split data by commas to get seperate IDs - Done
+2) find a way to read the individual lines up until the - seperatator to retrieve my first value, everything after the comma is my second value - Done
+3) Check if the first or second value starts with 0 if not then: - Done
+4) range through all values in the starting first value up to second value + 1
+5) How would I check for a repeated digit? What if the first half of each string = second half of each string?  e.g 555555 = true , 556557 = false. 77777777 true, 666777 false
+ 
+"""
+
+
+def check_repeated_digits(list_of_id_components):
+    """
+    Checks for repeated digits within a 3 - tuple list.
+    
+    :param (list_of_id_components):  [('11','-','22'), ('332','-','400'), ('222220','-','222224')]
+    :returns: sum_of_correct_values(int) : Summing a list of each repeated digit. 
+    """
+    
+    number_of_invalid_values = 0
+    list_of_invalids = [] 
+
+    for ids in list_of_id_components:
+        if ids[0][0] == "0" or ids[2][0] == "0": # If the first character of the first digit string == "0" or the first character of the second digit string == "0" add it to the total invalid values 
+            number_of_invalid_values += 1
+
+        else:
+            for values in range(int(ids[0]),int(ids[2]) + 1):
+                str_values = str(values)
+                str_value_length = len(str(values))
+                mid_point = str_value_length // 2
+                
+                
+                if str_values[:mid_point] == str_values[mid_point:]: #Check if the first half and second half of the string are the same if so this is an invalid didgit
+                    number_of_invalid_values += 1
+                list_of_invalids.append(int(str_values))
+           
+    sum_of_correct_values = sum(list_of_invalids)
+                    
+                            
+    return sum_of_correct_values
+
+
+
+
+#print(check_repeated_digits(better_split_id(data)))
+#print(split_id(test_data))
