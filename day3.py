13d434faa6ab1533365418c8c6d44298a90887b7
osman705 <ohassan705@gmail.com>
2025-12-15 02:29:24 +0000

    Completed P1 Day3
    Refactor find_largest_joltage function to calculate total based on individual batteries in banks.

--- day3.py
+++ day3.py
@@ -6,38 +6,44 @@ with open("day3data.txt") as file:
 #print(data)
 
 
-
-test_data = '987654321111111'
-
-
-
 def find_largest_joltage(data):
     """
-    Break data into a list of strings(banks) each bank is composed of batteries(chars)
+    Break data into a list of strings(banks), each bank is composed of batteries(chars)
     
     :param data: Description is a list of strings.
     returns: Int: Total of all (top 2 batteries in each bank)
     """
 
-    #How do I find the top two ? I check each bank and split the bank into specific batteries
-    # convert the batteries into a integers and sort the list. Add the last two digits together and add to the total
-    #move onto the next bank etc until completion. 
+    total = 0
 
-    total_jolt_count = 0
-    list_of_individual_banks = data.strip().split('\n') 
-    list_of_integer_batteries = []
-    largest_pairs = []
+    list_of_individual_banks = data.split('\n') 
+    banks_of_individual_int_batteries = []
+    current_bank = []
 
     for bank in list_of_individual_banks:
-        #for battery in bank:
-        list_of_integer_batteries.append(int(bank))
-        list_of_integer_batteries.sort()
-        largest_pairs.append(list_of_integer_batteries[-2:])
-    
-    
+        for battery in bank:
+            current_bank.append(int(battery))
+        
+        banks_of_individual_int_batteries.append(current_bank)
+        current_bank = []
+
+    for batteries in banks_of_individual_int_batteries:
+        if batteries[-1] != max(batteries):
+            index_of_first_largest = batteries.index(max(batteries))
+            largest_num = max(batteries)
+            total += largest_num * 10 
+            
+            second_largest = max(batteries[index_of_first_largest + 1:])
+            total += second_largest
+            
+        else:
+            largest_num = batteries[-1]
+            total += largest_num 
+            total += max(batteries[:-1]) * 10 
     
 
-    return largest_pairs
+    return total
+
 
 
 #print(find_largest_joltage(data))
